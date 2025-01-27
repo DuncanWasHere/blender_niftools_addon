cd538dcb5a0b6aaf619c284fb81d2ee04b668a29
DuncanWasHere <duncanhoward04@gmail.com>
2025-01-27 17:07:02 -0500

    Update divide by zero check for new glossiness export formula

--- io_scene_niftools/modules/nif_export/property/material.py
+++ io_scene_niftools/modules/nif_export/property/material.py
@@ -112,8 +112,9 @@ class MaterialProperty:
                  n_ni_material_property.emissive_color.b, _) = b_shader_node.inputs[27].default_value
 
             # Map roughness [0,1] to glossiness (MW -> 0.0 - 128.0)
-            if not b_shader_node.inputs[2].default_value == 0:
-                n_ni_material_property.glossiness = 2 / (1 - b_shader_node.inputs[2].default_value ** 0.5)
+            glossiness = (1 - b_shader_node.inputs[2].default_value ** 0.5)
+            if not glossiness == 0:
+                n_ni_material_property.glossiness = 2 / glossiness
             else:
                 n_ni_material_property.glossiness = 0
 
